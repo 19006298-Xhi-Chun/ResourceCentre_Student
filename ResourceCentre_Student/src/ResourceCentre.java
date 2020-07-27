676298f79bd829f2c028c26950f8ce05fe4333b8
19006298 <19006298@LAPTOP-S5V95G1P>
2020-07-27 14:29:39 +0800

    LoadChrombook

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -212,10 +212,28 @@ public class ResourceCentre {
 	
 	public static boolean doLoanChromebook(ArrayList<Chromebook> chromebookList, String tag, String dueDate) {
 		// write your code here
-		return true;
+		boolean Loaned=false;
+		for (int i=0; i< chromebookList.size() ; i++) {
+			if(tag.equalsIgnoreCase(chromebookList.get(i).getAssetTag())&& chromebookList.get(i).getIsAvailable() ==true) {
+		chromebookList.get(i).setIsAvailable(false);
+		chromebookList.get(i).setDueDate(dueDate);
+			
+			}	
+			}
+		return Loaned;
+		
 	}
 	public static void loanChromebook(ArrayList<Chromebook> chromebookList) {
 		// write your code here
+		ResourceCentre.viewAllChromebook(chromebookList);
+		String tag = Helper.readString("Enter asset tag > ");
+		String due = Helper.readString("Enter due date > ");
+		Boolean isLoaned = doLoanChromebook(chromebookList, tag, due);
+		if (isLoaned == false) {
+			System.out.println("Invalid asset tag");
+		} else {
+			System.out.println("Chromebook " + tag + " loaned out");
+		}
 		
 		
 	}
